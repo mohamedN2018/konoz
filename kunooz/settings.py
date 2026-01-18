45cf41f534a10a184b9cd7938d3df0fcbaac7f1b
mohamedN2018 <moh24832@gmail.com>
2026-01-18 11:43:01 +0200

    chenge db

--- kunooz/settings.py
+++ kunooz/settings.py
@@ -12,8 +12,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # BASE & SECURITY
 # =========================
 
-SECRET_KEY = config('MY_SECRET_KEY')
-DEBUG = True
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', cast=bool)
 
 
 MAIN_DOMAIN = config("MAIN_DOMAIN", default="kunooz.deplois.net").replace("https://", "").replace("http://", "")
@@ -103,13 +103,23 @@ WSGI_APPLICATION = 'kunooz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': config('DB_ENGINE'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT', cast=int),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
